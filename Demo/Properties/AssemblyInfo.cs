d1615dc041ab7b1bfa25e7dab04c46821882a63e
Danila Korablin <AlphaOmega@narod.ru>
2023-07-06 18:26:28 +0300

    YML: Added submodule synchronization

--- Demo/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: Guid("46b6d6e1-fc03-4c3e-9934-4652cb2919d4")]
--- ElfReader/Properties/AssemblyInfo.cs
+++ ElfReader/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 
 #if !NETSTANDARD
 [assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016")]
+[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2023")]
 [assembly: AssemblyProduct("Executable and Linkable Format Reader")]
 [assembly: AssemblyTitle("ELF Reader")]
 #if DEBUG
